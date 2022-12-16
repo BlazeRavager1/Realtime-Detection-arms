40a53dbab401c65f1ed6cadf93d54c4523cced99
BlazeRavager1 <89845937+BlazeRavager1@users.noreply.github.com>
2022-12-16 17:46:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+noseX = 0;
+noseY = 0;
+
+Difference = 0;
+rightWrist = 0;
+leftWrist = 0;
+
 function setup() {
     video = createCapture(VIDEO);
     video.size(550, 500);
@@ -15,10 +22,23 @@ function modelLoaded() {
 
 function draw() {
     background("#FF0000");
+    stroke('#FF0000');
+    square(noseX, noseY, Difference);
 }
 
 function gotPoses(results) {
     if(results.length > 0){
         console.log(results);
+
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        leftWrist = results[0].pose.leftWrist.x;
+        rightWrist = results[0].pose.rightWrist.x;
+        Difference = floor(leftWrist - rightWrist);
+
+        console.log("Left Wrist X = " + leftWrist + "Right Wrist X = " + rightWrist + "The differnce between left and right wrist is = " + Difference);
     }
 }
